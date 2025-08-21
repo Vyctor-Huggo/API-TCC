c62ab0c2fb63ad785462be9f7801ba98fb4f04d9
Vyctor Huggo <74301628+Vyctor-Huggo@users.noreply.github.com>
2025-08-20 21:29:04 -0300

    Create migration.sql

---
+++ prisma/migrations/20250820224200_ww/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "energy_equipments" ALTER COLUMN "time" SET DATA TYPE DOUBLE PRECISION;
+
+-- AlterTable
+ALTER TABLE "water_equipments" ALTER COLUMN "time" SET DATA TYPE DOUBLE PRECISION;
